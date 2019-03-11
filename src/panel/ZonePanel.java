5e060baae79f741b0c374c8452dc0f0140314c54
emmiage <48280803+emmiage@users.noreply.github.com>
2019-03-11 15:45:34 +0100

    Update ZonePanel.java

--- src/panel/ZonePanel.java
+++ src/panel/ZonePanel.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Vector;
 
 import elmtJeu.Etape;
-import main.Jeu;
+import elmtJeu.Joueur;
 import utils.Zone;
 
 //c'est le paneau qui contiendra les images des zones et qui permettra de les faire défiler 
@@ -17,7 +17,7 @@ public class ZonePanel extends Panel {
 
 		
 	public ZonePanel(String nvImage,int nvLargeur, int nvLongueur) throws IOException {
-		super(nvImage,nvLargeur, nvLongueur);
+		super(nvImage,nvLargeur, nvLongueur, null);
 		
 		this.setLayout(new CardLayout());
 		this.ajouterZone();
@@ -27,14 +27,14 @@ public class ZonePanel extends Panel {
 	public void ajouterZone() throws IOException {
 		//ajout de la zone couloir qui our l'instant represente tous ce qui n'est pas une piece
 		this.vZone = new Vector<Zone>();
-		Zone nvZone = new Zone("entree",ZonePanel.nbZone,"entree", 0, 0, 0, 0, null, null);
+		Zone nvZone = new Zone("couloir",ZonePanel.nbZone,"couloir", 0, 0, 0, 0, null, null);
 		this.vZone.add(nvZone);
-		this.add(new Panel(nvZone.getImgSource(), this.largeur,this.longueur), nvZone.getNom());
+		this.add(new Panel(nvZone.getImgSource(), this.largeur,this.longueur,null), nvZone.getNom());
 		ZonePanel.nbZone ++;
 		
 		
-		BufferedReader infoZones = new BufferedReader(new FileReader("src/zones.txt"));
-		this.add(new Panel("blanc", 600,450), "blanc");
+		BufferedReader infoZones = new BufferedReader(new FileReader("src/files/zones.txt"));
+		this.add(new Panel("blanc", 600,450,null), "blanc");
 		String nom;
 		Integer minX;
 		Integer maxX;
@@ -42,12 +42,12 @@ public class ZonePanel extends Panel {
 		Integer maxY;
 		String lineInfo = infoZones.readLine();
 		
-		BufferedReader etape = new BufferedReader( new FileReader("src/etapes.txt"));
+		BufferedReader etape = new BufferedReader( new FileReader("src/files/etapes.txt"));
 		String lineEtape = etape.readLine();
 		int numEtape;
 		Vector<Etape> vEtapes;
 		
-		BufferedReader instructions = new BufferedReader( new FileReader("src/instructions.txt"));
+		BufferedReader instructions = new BufferedReader( new FileReader("src/files/instructions.txt"));
 		String lineInstructions = instructions.readLine();
 		Vector<String> vInstructions;
 		
@@ -82,26 +82,22 @@ public class ZonePanel extends Panel {
 			
 			nvZone = new Zone(nom,ZonePanel.nbZone,nom, minX, maxX, minY, maxY, vEtapes, vInstructions);
 			this.vZone.add(nvZone);
-			this.add(new Panel(nvZone.getImgSource(), this.largeur,this.longueur), nvZone.getNom());
+			this.add(new Panel(nvZone.getImgSource(), this.largeur,this.longueur,null), nvZone.getNom());
 			lineInfo = infoZones.readLine();
 		}
 	}
 	
 	//nomzonecourante
-	public Zone getCurrentZone() {
+	public Zone getCurrentZone(Joueur joueur) {
 		for(int i=1;i<ZonePanel.nbZone;i++) {
-			if(Jeu.joueur.getPosX()>vZone.get(i).getDim().get(0) && Jeu.joueur.getPosX() < vZone.get(i).getDim().get(1) ) {
-				if(Jeu.joueur.getPosY() > vZone.get(i).getDim().get(2) && Jeu.joueur.getPosY() < vZone.get(i).getDim().get(3)) {
+			if(joueur.getPosX()>vZone.get(i).getDim().get(0) && joueur.getPosX() < vZone.get(i).getDim().get(1) ) {
+				if(joueur.getPosY() > vZone.get(i).getDim().get(2) && joueur.getPosY() < vZone.get(i).getDim().get(3)) {
 					return vZone.get(i);
 				}
 			}
 		}
 		return vZone.get(0);
 		//je suis dans un couloir dés que je ne suis pas dans une zone repertorié pour le moment
-	}
-	
-	public void setZoneImage(String nom) {
-		((CardLayout) this.getLayout()).show(this, nom);
 	}
 	
 }
